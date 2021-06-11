b0112ff7ec79b3e4ca05ca4c5f675b213d427690
VikasKumar75 <mr.rajput7547@gmail.com>
2021-06-12 05:26:31 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InfiniteList(),
+    );
+  }
+}
+class InfiniteList extends StatelessWidget {
+  const InfiniteList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
